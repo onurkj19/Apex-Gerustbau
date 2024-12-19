e4ee0b264e37e257b2a5b23c20333f669688ccbf
onurkj19 <onurkj19@gmail.com>
2024-12-19 23:29:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Hamburger Menu Toggle
+// Marrim butonin e hamburger dhe lidhjet
 const hamburger = document.getElementById("hamburger-menu");
 const navLinks = document.querySelector(".nav-links");
 
+// Kur klikohet butoni, aktivizo dhe çaktivizo klasën "active"
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
-    hamburger.classList.toggle("active");
 });
